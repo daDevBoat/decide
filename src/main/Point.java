e6fa6ec35762b007b62c7e51f95b808c1c9ce804
daDevBoat <elias.richard.naess@gmail.com>
2026-01-21 17:42:44 +0100

    feat Add Distance Method for Point Class
    Added the distance method for the point class. It is static and takes two points.
    
    Closes #22

--- src/main/Point.java
+++ src/main/Point.java
@@ -6,4 +6,8 @@ public class Point {
         this.x = x;
         this.y = y;
     } 
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    }
 }
