05b051a064b6d237b95b2cda2471991cc48b201b
Arnau Pelechano García <arnaupg@kth.se>
2026-01-22 10:16:05 +0100

    feat Add Triangle Area Method to Point Class (#23)

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.*;
+
 public class Point {
     double x;
     double y;
@@ -9,5 +11,10 @@ public class Point {
 
     public static double distance(Point p1, Point p2) {
         return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    }
+
+    public static double triangleArea(Point p1, Point p2, Point p3){
+        double c = p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y);
+        return Math.abs(c) / 2.0;
     }
 }
