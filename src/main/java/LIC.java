332964cbe86dac77c457ac054228652161f7a7ff
daDevBoat <elias.richard.naess@gmail.com>
2026-01-26 12:31:02 +0100

    feat: change all LIC method parameters and change unit tests to accomodate parameters change for LICs
    close #72

--- src/main/java/LIC.java
+++ src/main/java/LIC.java
@@ -1,26 +1,26 @@
 public class LIC {
 
-    public static boolean LIC0(int numPoints, Point[] points, double LENGTH1) {
+    public static boolean LIC0(int numPoints, Point[] points, Parameters p) {
         for (int i = 0; i < numPoints - 1; i++) {
-            if (Point.distance(points[i], points[i + 1]) > LENGTH1)
+            if (Point.distance(points[i], points[i + 1]) > p.LENGTH1)
                 return true;
         }
         return false;
     }
 
-    public static boolean LIC1(int numPoints, Point[] points, double RADIUS1) {
-        if (numPoints < 3 || RADIUS1 < 0)
+    public static boolean LIC1(int numPoints, Point[] points, Parameters p) {
+        if (numPoints < 3 || p.RADIUS1 < 0)
             return false;
         for (int i = 0; i < numPoints - 2; i++) {
-            if (Point.circleRadius(points[i], points[i + 1], points[i + 2]) > RADIUS1)
+            if (Point.circleRadius(points[i], points[i + 1], points[i + 2]) > p.RADIUS1)
                 return true;
         }
         return false;
     }
 
-    public static boolean LIC2(int numPoints, Point[] points, double EPSILON) {
+    public static boolean LIC2(int numPoints, Point[] points, Parameters p) {
         double PI = 3.14159;
-        if (!(0 <= EPSILON && EPSILON < PI)) {
+        if (!(0 <= p.EPSILON && p.EPSILON < PI)) {
             return false;
         }
         if (numPoints == 2) {
@@ -32,7 +32,7 @@ public class LIC {
             Point p3 = points[i + 2];
             if (!((p1.x == p2.x && p1.y == p2.y) || (p3.x == p2.x && p3.y == p2.y))) {
                 double angle = Point.angle(p1, p2, p3);
-                if (angle < PI - EPSILON || angle > PI + EPSILON) {
+                if (angle < PI - p.EPSILON || angle > PI + p.EPSILON) {
                     return true;
                 }
             }
@@ -40,17 +40,17 @@ public class LIC {
         return false;
     }
 
-    public static boolean LIC6(int numPoints, Point[] points, int N_PTS, double DIST) {
-        if (numPoints < 3 || N_PTS < 3 || DIST < 0 || numPoints < N_PTS)
+    public static boolean LIC6(int numPoints, Point[] points, Parameters p) {
+        if (numPoints < 3 || p.N_PTS < 3 || p.DIST < 0 || numPoints < p.N_PTS)
             return false;
 
-        for (int i = 0; i <= numPoints - N_PTS; i++) {
+        for (int i = 0; i <= numPoints - p.N_PTS; i++) {
             Point p_start = points[i];
-            Point p_end = points[i + N_PTS - 1];
+            Point p_end = points[i + p.N_PTS - 1];
 
-            for (int j = i + 1; j <= i + N_PTS - 2; j++) {
+            for (int j = i + 1; j <= i + p.N_PTS - 2; j++) {
                 Point p_mid = points[j];
-                if (Point.distancePointToLine(p_start, p_end, p_mid) > DIST) {
+                if (Point.distancePointToLine(p_start, p_end, p_mid) > p.DIST) {
                     return true;
                 }
             }
@@ -69,25 +69,25 @@ public class LIC {
         return false;
     }
 
-    public static boolean LIC7(int numPoints, Point[] points, double LENGTH1, int K_PTS) {
-        if (numPoints < 3 || K_PTS < 1 || K_PTS > numPoints - 2)
+    public static boolean LIC7(int numPoints, Point[] points, Parameters p) {
+        if (numPoints < 3 || p.K_PTS < 1 || p.K_PTS > numPoints - 2)
             return false;
-        for (int i = 0; i < numPoints - K_PTS - 1; i++) {
-            if (Point.distance(points[i], points[i + K_PTS + 1]) > LENGTH1)
+        for (int i = 0; i < numPoints - p.K_PTS - 1; i++) {
+            if (Point.distance(points[i], points[i + p.K_PTS + 1]) > p.LENGTH1)
                 return true;
         }
         return false;
     }
 
-    public static boolean LIC8(int numPoints, Point[] points, double RADIUS1, int A_PTS, int B_PTS) {
-        if (A_PTS < 1 || B_PTS < 1 || numPoints < 5 || A_PTS+B_PTS > (numPoints-3)){
+    public static boolean LIC8(int numPoints, Point[] points, Parameters p) {
+        if (p.A_PTS < 1 || p.B_PTS < 1 || numPoints < 5 || p.A_PTS+p.B_PTS > (numPoints-3)){
             return false;
         }
-        for (int i = 0; i + A_PTS+B_PTS < numPoints-2; i++){
+        for (int i = 0; i + p.A_PTS+p.B_PTS < numPoints-2; i++){
             Point p1 = points[i];
-            Point p2 = points[i+A_PTS+1];
-            Point p3 = points[i+A_PTS+B_PTS+2];
-            if (Point.circleRadius(p1, p2, p3) > RADIUS1){
+            Point p2 = points[i+p.A_PTS+1];
+            Point p3 = points[i+p.A_PTS+p.B_PTS+2];
+            if (Point.circleRadius(p1, p2, p3) > p.RADIUS1){
                 return true;
             }
         }
@@ -117,28 +117,28 @@ public class LIC {
         return false;
     }
 
-    public static boolean LIC10(int numPoints, Point[] points, int E_PTS, int F_PTS, double AREA1) {
-        if (numPoints < 5 || E_PTS < 1 ||F_PTS < 1 || E_PTS + F_PTS > numPoints - 3) return false;
+    public static boolean LIC10(int numPoints, Point[] points, Parameters p) {
+        if (numPoints < 5 || p.E_PTS < 1 ||p.F_PTS < 1 || p.E_PTS + p.F_PTS > numPoints - 3) return false;
 
-        for (int i = 0; i < numPoints - F_PTS - E_PTS - 2; i ++) {
+        for (int i = 0; i < numPoints - p.F_PTS - p.E_PTS - 2; i ++) {
             Point p1 = points[i];
-            Point p2 = points[i + E_PTS + 1];
-            Point p3 = points[i + E_PTS + F_PTS + 2];
+            Point p2 = points[i + p.E_PTS + 1];
+            Point p3 = points[i + p.E_PTS + p.F_PTS + 2];
             
-            if (Point.triangleArea(p1, p2, p3) > AREA1) {
+            if (Point.triangleArea(p1, p2, p3) > p.AREA1) {
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean LIC11(int numPoints, Point[] points , int G_PTS){
-        if (numPoints < 3 || 1 > G_PTS || G_PTS > numPoints-2){
+    public static boolean LIC11(int numPoints, Point[] points , Parameters p){
+        if (numPoints < 3 || 1 > p.G_PTS || p.G_PTS > numPoints-2){
             return false;
         }
-        for (int i = 0; i + G_PTS < numPoints-1; i++){
+        for (int i = 0; i + p.G_PTS < numPoints-1; i++){
             Point p1 = points[i];
-            Point p2 = points[i+G_PTS+1];
+            Point p2 = points[i+p.G_PTS+1];
             if (p2.x - p1.x < 0){
                 return true; 
             }
@@ -167,14 +167,14 @@ public class LIC {
     }
 
 
-    public static boolean LIC13(int numPoints, Point[] points, double RADIUS1, int A_PTS, int B_PTS, double RADIUS2) {
-        if (numPoints < 5 || RADIUS2 < 0) return false;
+    public static boolean LIC13(int numPoints, Point[] points, Parameters p) {
+        if (numPoints < 5 || p.RADIUS2 < 0) return false;
         boolean not_contained_flag = false, contained_flag = false;
-        for (int i = 0; i < numPoints - A_PTS - B_PTS - 2; i++) {
-            double radius = Point.circleRadius(points[i], points[i + A_PTS + 1], points[i + A_PTS + B_PTS + 2]);
+        for (int i = 0; i < numPoints - p.A_PTS - p.B_PTS - 2; i++) {
+            double radius = Point.circleRadius(points[i], points[i + p.A_PTS + 1], points[i + p.A_PTS + p.B_PTS + 2]);
             
-            if (radius > RADIUS1) not_contained_flag = true;
-            if (radius <= RADIUS2) contained_flag = true;
+            if (radius > p.RADIUS1) not_contained_flag = true;
+            if (radius <= p.RADIUS2) contained_flag = true;
 
             if (not_contained_flag && contained_flag) return true;
         }
